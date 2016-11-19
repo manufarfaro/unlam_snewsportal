88e3fd680803f0523886194932c9b8e0a18ce613
Manu Farfaro <manufarfaro@gmail.com>
2016-11-19 05:39:12 -0300

    set database encoding to utf8

--- classes/model/ConnectionProvider.class.php
+++ classes/model/ConnectionProvider.class.php
@@ -28,6 +28,7 @@
                 try {
                     $this->instance = new PDO($this->connectionString, $this->user, $this->password);
                     $this->instance->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT);
+                    $this->instance->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, "SET NAMES 'utf8'");
                 } catch (PDOException $e) {
                     echo "Connection Database Error: " . $e->getMessage();
                 }
