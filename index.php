bdfaf90d0a76e222dac627ff7240bc1caf7f2681
Manu Farfaro <manufarfaro@gmail.com>
2016-11-19 05:45:52 -0300

    title changed.

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <!doctype html>
 <html>
 <head>
-    <title>TP</title>
+    <title>SNewsPortal</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link type="text/css" rel="Stylesheet" charset="utf-8" href="./css/Styles.css" />
